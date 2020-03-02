2e85809eced9cd436096285f4e3fa7c9bfc3f68e
Daniel Saez <dsaez20318@columbushs.com>
2020-03-01 21:19:49 -0500

    html no buttons

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let stopwatch = document.getElementById(Stopwatch)
 
-document.addEventListener.stopwatch
+let PlusRedOrangeCube = document.getElementById("PlusRedOrangeCube");
+let PlusRedGreenCube
+let PlusRedPurpleCube
+
+let MinusRedOrangeCube = document.getElementById("MinusRedOrangeCube");
+let MinusRedGreenCube
+let MinusRedPurpleCube
+
+let PlusBlueOrangeCube
+let PlusBlueGreenCube
+let PlusBluePurpleCube
+
+let MinusBlueOrangeCube
+let MinusBlueGreenCube
+let MinusBluePurpleCube
+
+
+document.querySelector("").addEventListener(‘click’, function(e) {
+
+        // do whatever you want here
+        
+        }
